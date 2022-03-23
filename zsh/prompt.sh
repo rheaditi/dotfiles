b5dc9238e6d5cc663de5cf109dc8cfeaf14e878c
Aditi Mohanty <rheaditi@users.noreply.github.com>
2022-03-23 12:19:04 +0530

    Fix prompt & vscode theme update

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,26 +1,4 @@
-# SPACESHIP_PROMPT_ORDER=(
-#   user
-#   dir
-#   git_branch
-#   git_status
-#   line_sep
-#   char
-# )
-
-# SPACESHIP_CHAR_SYMBOL="❯"
-# SPACESHIP_CHAR_SUFFIX=" "
-# SPACESHIP_GIT_BRANCH_PREFIX=""
-# # SPACESHIP_GIT_BRANCH_SUFFIX=' '
-# SPACESHIP_GIT_BRANCH_COLOR="240"
-# SPACESHIP_DIR_COLOR='magenta'
-# SPACESHIP_GIT_STATUS_COLOR='061'
-
+# pure-prompt
+fpath+=('/Users/amohanty/.nvm/versions/node/v17.8.0/lib/node_modules/pure-prompt/functions')
 autoload -U promptinit; promptinit
-prompt pure # pure prompt
-# prompt spaceship # Set Spaceship ZSH as a prompt
-
-# https://github.com/denysdovhan/spaceship-prompt/issues/426#issuecomment-576036367
-# () {
-#   local z=$'\0'
-#   PROMPT='${${${$(spaceship_prompt)//\%\%/'$z'}//\%B}//'$z'/%%}'
-# }
+prompt pure
