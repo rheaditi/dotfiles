dfc0f9e2afa7e4c6c56abedb6e02e54b21417617
Aditi Mohanty <rheaditi@users.noreply.github.com>
2023-06-05 12:27:42 +0530

    chore: Run prompt when not Warp

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# pure-prompt
-fpath+=("$(brew --prefix)/share/zsh/site-functions")
-autoload -U promptinit; promptinit
-prompt pure
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+  # pure-prompt
+  fpath+=("$(brew --prefix)/share/zsh/site-functions")
+  autoload -U promptinit; promptinit
+  prompt pure
+fi
