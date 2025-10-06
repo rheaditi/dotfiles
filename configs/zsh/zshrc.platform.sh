173f645587604619ec70b9bab0cba64ec8076658
Aditi Mohanty <rheaditi@users.noreply.github.com>
2025-10-06 19:00:45 +0530

    add platform configs

---
+++ configs/zsh/zshrc.platform.sh
@@ -0,0 +1,8 @@
+# Common for both macos and linux but differentiates local vs devbox
+
+if is-local; then
+  # nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
